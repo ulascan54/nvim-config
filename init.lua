0905a3415ab5bd173a0645b1565e3fd1e92ec281
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2025-10-10 01:13:53 +0300

    highlighting functionality (nvim-treesitter)

--- init.lua
+++ init.lua
@@ -20,7 +20,8 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } }
+  { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-treesitter/nvim-treesitter", build =":TSUpdate"}
 }
 
 local opts = {}
@@ -31,6 +32,12 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript"},
+  highlight = { enable = true },
+  indent = { enable =true},
+})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
